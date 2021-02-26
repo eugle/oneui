c91229c12fcce36a6c6c7c73fa217369d92baba3
eugle <eugle@protonmail.com>
2021-02-26 13:52:52 +0800

    优化 column getGradient

--- src/components/internal/getGradient.js
+++ src/components/internal/getGradient.js
@@ -5,9 +5,10 @@ import {DefaultColor as Color} from "../../styles/color";
 import color from '../basic/color';
 
 export const getGradient = bg => {
-    const color = Object.keys(color).includes(bg) ? Color[bg] : bg;
-    return [
-        Object.keys(color).includes(bg) ? Color[bg] : bg,
-        Object.keys(color).includes(bg) ? Color[bg + 'G'] : bg + 'G'
-    ];
+    if (bg && typeof bg === "string") {
+        return [
+            Object.keys(color).includes(bg) ? Color[bg] : bg,
+            Object.keys(color).includes(bg) ? Color[bg + 'G'] : bg + 'G'
+        ];
+    }
 };
--- src/components/internal/setBackground.js
+++ src/components/internal/setBackground.js
@@ -7,10 +7,6 @@ export const setBackground = (bg, g) => {
     const [gs, ge] = getGradient(bg);
     return `
         background-color: ${gs};
-        background-image: -webkit-linear-gradient(${g}deg, ${gs} 0%, ${ge} 100%);
-        background-image: -moz-linear-gradient(${g}deg, ${gs} 0%, ${ge} 100%);
-        background-image: -o-linear-gradient(${g}deg, ${gs} 0%, ${ge} 100%);
-        background-image: -ms-linear-gradient(${g}deg, ${gs} 0%, ${ge} 100%);
         background-image: linear-gradient(${g}deg, ${gs} 0%, ${ge} 100%);
     `;
 };

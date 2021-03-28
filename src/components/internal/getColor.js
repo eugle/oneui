e2ea708d83f6b856fc88c6d4f948fff55695d66c
eugle <eugle@protonmail.com>
2021-03-28 16:45:47 +0800

    修复改变默认颜色不生效问题

--- src/components/internal/getColor.js
+++ src/components/internal/getColor.js
@@ -3,6 +3,7 @@
  * c,o,over:是否激活颜色
  * */
 import {DefaultColor as Color} from '../../styles/color';
+import color from '../basic/color';
 
 export const getColor = (c, o, over) => {
     //判断透明色
@@ -11,18 +12,18 @@ export const getColor = (c, o, over) => {
     let currentColor;
     //如果是 0,0,0 并指定透明度，直接组合返回;
     if (c && String(c).includes(',') && !String(c).includes('rgba')) {
-        currentColor = `rgba(${c},${opacity})`;
+        c = `rgba(${c},${opacity})`;
     }
 
     //如果是 16进制 并指定透明度，不处理直接返回，输出提示;
     if (c && String(c).includes('#') && (o === 0 || o)) {
         console.log('#,0');
-        currentColor = c;
     }
 
     //如果是默认单或双数值颜色，判断并组合;
-    if (c && c.length <= 2 && Object.keys(Color).includes(c)) {
-        currentColor = o || o === 0 ? `rgba(${Color[`${c}RGB`]},${opacity})` : over ? Color[`${c}G`] : Color[c];
+    if (c && c.length <= 2) {
+        const current = Object.keys(color).includes(c) ? color[c] : c;
+        c = o || o === 0 ? `rgba(${Color[`${current}RGB`]},${opacity})` : over ? Color[`${current}G`] : Color[current];
     }
 
     //如果同时给了颜色和透明度 [bg,bgo]
@@ -30,18 +31,19 @@ export const getColor = (c, o, over) => {
         const [cc, co] = c;
         //如果背景是默认单或双数值颜色，判断并组合
         if (Object.keys(Color).includes(cc)) {
-            currentColor = `rgba(${Color[`${cc}RGB`]},${co || 1})`
+            const current = Object.keys(color).includes(cc) ? color[cc] : cc;
+            c = co || co === 0 ? `rgba(${Color[`${current}RGB`]},${opacity})` : Color[current];
         }
         //如果是rgba色
         if (String(cc).includes(',') && !String(cc).includes('rgba')) {
-            currentColor = `rgba(${cc},${co || 1})`;
+            c = `rgba(${cc},${co || 1})`;
         }
 
         //如果是#十六进制色
         if (String(cc).includes('#')) {
-            currentColor = cc;
+            c = cc;
         }
     }
 
-    return currentColor || '';
+    return c || '';
 };
